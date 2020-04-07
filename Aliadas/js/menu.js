3682866a0d7c3e1a9723c7f9ce22a61ba72cb072
roberto-abt <52540705+roberto-abt@users.noreply.github.com>
2020-04-06 21:32:16 -0400

    Add files via upload

---
+++ Aliadas/js/menu.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    var header = document.getElementById('header');
+    var headroom = new Headroom(header);
+    headroom.init();
+
+    //menu responsive
+    //calculamos el ancho de la pagina
+    var ancho = $(window).width().
+        enlaces = $('#enlaces-t').
+        btnmenu = $('#btn-menu').
+        icono = $('#btn-menu .icono-1');
+
+        if(ancho < 700){
+            enlaces.hide();
+        }
+});
